663aa3258c364bf06d86b914842779d717e95309
Anib <trainee.anib.mhamud@gmail.com>
2026-02-27 17:28:41 +0000

    Done but not done

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -4,23 +4,28 @@ Note: Do not add ANY variables to the global scope. This WILL break the tests.
 
 
 def generate_invoice(receipt_string: str) -> str:
+    """Function changes the receipt into an invoice"""
     if receipt_string == "" or receipt_string == "Total: £0.00":
         return ("VAT RECEIPT\n\nTotal: £0.00\nVAT: £0.00\nTotal inc VAT: £0.00")
 
     receipt_split = receipt_string.split("\n")
 
+    invoice = "VAT RECEIPT\n\n"
     line_for_total = ""
     for line in receipt_split:
         if line[:5] == "Total":
             line_for_total += line
             break
     list_of_prices = []
-    for i in range(receipt_split):
-        price_without_tax = float(receipt_split[i][-4:]) * 0.8
+    for line in receipt_split:
+        price_without_tax = float(line[-4:]) * 0.8
         list_of_prices.append(price_without_tax)
+        if line[:5] == "Total":
+            line_for_total += line
+            break
 
     print(f"{price_without_tax:.2f}")
-    return  # return the invoice string
+    return invoice  # return the invoice string
 
 
 if __name__ == "__main__":
