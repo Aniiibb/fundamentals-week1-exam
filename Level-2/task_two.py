e0b7af0bac4fb65e2fcbc265e1a452cb382d9fbe
Anib <trainee.anib.mhamud@gmail.com>
2026-02-27 16:14:45 +0000

    Level 2 Base Functionalities

--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -13,12 +13,44 @@ basket = []
 
 
 def add_to_basket(item: dict) -> list:
-    pass
+    """Adds new item to basket and groups identical items"""
+    item_name = item["name"]
+    item_price = item["price"]
 
+    for basket_item in basket:
+        if basket_item["name"] == item_name and basket_item["price"] == item_price:
+            basket_item["count"] += 1
+            return basket
+
+    basket.append({
+        "name": item_name,
+        "price": item_price,
+        "count": 1
+    })
 
 
 def generate_receipt(basket: list) -> str:
-    pass
+    total = 0
+    receipt = ""
+    if basket == []:
+        return ("Basket is empty")
+
+    for item in basket:
+        name = item["name"]
+        price = item["price"]
+        quantity = item["count"]
+
+        line_total = price * quantity
+        total += line_total
+
+        if price == 0:
+            line = f"{name} x {quantity} - Free\n"
+        else:
+            line = f"{name} x {quantity} - £{line_total:.2f}\n"
+
+    receipt += f"Total: £{total:.2f}"
+    return receipt  # return the receipt string
+
 
 #####
 #
